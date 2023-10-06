7eece42206afcf5e974f1ef78b598a2456df6f33
Enes Dolgun <58950702+dlgn2@users.noreply.github.com>
2023-10-06 11:18:18 +0300

    Nodeified

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 /**
  * @format
  */
-
+import './shim.js';
+import 'reflect-metadata';
+import setGlobalVars from 'indexeddbshim/dist/indexeddbshim-noninvasive';
+import SQLite from 'react-native-sqlite-2';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+setGlobalVars(global, {
+  checkOrigin: false,
+  win: SQLite,
+});
 AppRegistry.registerComponent(appName, () => App);
